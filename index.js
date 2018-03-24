ba6693d5662f57f7ec000aa8e5c7dadcd3bd6774
Martin Fieber <martin.fieber@sinnerschrader.com>
2018-03-24 16:23:37 +0100

    refactor: show video image

--- index.js
+++ index.js
@@ -4,19 +4,27 @@
 
 const rs2 = require('node-librealsense');
 const cv = require('opencv4nodejs');
+
 const pipeline = new rs2.Pipeline();
 
-pipeline.start();
+// const config = new rs2.Config();
+// config.enableStream(rs2.stream.STREAM_COLOR, -1, 640, 480, rs2.format.FORMAT_RGB8, 30);
 
-// const rows = 720;
-// const cols = 1280;
+pipeline.start(/* config */);
+
+// if (!config.canResolve(pipeline)) {
+//     throw new Error('voll dooof!');
+// }
+
 const winName = 'bob';
 
 function frame() {
     const frameset = pipeline.waitForFrames();
     const depth = frameset.depthFrame;
+    const color = frameset.colorFrame;
+
     if (depth) {
-        const matFromArray = new cv.Mat(Buffer.from(depth.data), depth.height, depth.width, cv.CV_8SC1);
+        const matFromArray = new cv.Mat(Buffer.from(color.data), color.height, color.width, cv.CV_8UC3);
         cv.imshow(winName, matFromArray);
         cv.waitKey(1000 / 60);
     }
