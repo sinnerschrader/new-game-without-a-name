b944de384d2ccbbe62f7914d91a44939f8c976bd
Martin Fieber <martin.fieber@sinnerschrader.com>
2018-03-25 09:41:22 +0200

    fix: handle errors properly

--- index.js
+++ index.js
@@ -5,41 +5,48 @@
 const rs2 = require('node-librealsense');
 const cv = require('opencv4nodejs');
 
-const pipeline = new rs2.Pipeline();
-
-// const config = new rs2.Config();
-// config.enableStream(rs2.stream.STREAM_COLOR, -1, 640, 480, rs2.format.FORMAT_RGB8, 30);
-
-pipeline.start(/* config */);
-
-// if (!config.canResolve(pipeline)) {
-//     throw new Error('voll dooof!');
-// }
-
-const winName = 'bob';
-
-function frame() {
+/**
+ * Render a single frame
+ */
+function frame({ pipeline }) {
     const frameset = pipeline.waitForFrames();
-    const depth = frameset.depthFrame;
+    // const depth = frameset.depthFrame; // for use if necessary
     const color = frameset.colorFrame;
 
-    if (depth) {
+    if (color) {
         const matFromArray = new cv.Mat(Buffer.from(color.data), color.height, color.width, cv.CV_8UC3);
-        cv.imshow(winName, matFromArray);
+        cv.imshow('frame', matFromArray);
         cv.waitKey(1000 / 60);
     }
 }
 
-function stop() {
+/**
+ * Stop the application and close all windows
+ */
+function exit({ pipeline }) {
     pipeline.stop();
     pipeline.destroy();
     rs2.cleanup();
-    cv.destroyWindow(winName);
+    cv.destroyAllWindows();
 
     process.exit(0);
 }
 
-process.on('exit', stop);
-process.on('SIGINT', stop);
+try {
+    const pipeline = new rs2.Pipeline();
+    const config = new rs2.Config();
 
-setInterval(frame, 1);
+    config.enableStream(rs2.stream.STREAM_COLOR, 0, 640, 480, rs2.format.FORMAT_BGR8, 30);
+    pipeline.start(config);
+
+    if (!config.canResolve(pipeline)) {
+        throw new Error('Configuration can not be applied to pipeline!');
+    }
+
+    process.on('exit', () => exit({ pipeline }));
+    process.on('SIGINT', () => exit({ pipeline }));
+
+    setInterval(() => frame({ pipeline }), 1);
+} catch (ex) {
+    console.error(ex);
+}
